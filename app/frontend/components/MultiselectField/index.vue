5e433ebdccf70685df8076e549ab3cb097863465
maxBelokonskii <m.belokonskiy@trucker.group>
2026-04-02 22:33:23 +0300

    Migrate common components to Vue 3 Composition API
    - SurveyForm/Question: $emit('input') → emit('update:modelValue'),
      watch with deep option, <script setup>
    - SurveyForm/index: $notify → useNotification() from vue3-notification,
      validateForm computed → isFormValid, hasOwnProperty → Object.hasOwn
    - MultiselectField: vue-multiselect → @vueform/multiselect,
      $refs → template ref, mode="tags" for multi-select

--- app/frontend/components/MultiselectField/index.vue
+++ app/frontend/components/MultiselectField/index.vue
@@ -1,74 +1,54 @@
 <template>
   <div>
-    <multiselect v-model="selectValue"
-                 :options="options"
-                 :multiple="true"
-                 :close-on-select="false"
-                 :clear-on-select="false"
-                 :hide-selected="true"
-                 :preserve-search="true"
-                 placeholder="Выберите или начните вводить"
-                 label="name"
-                 track-by="name"
-                 select-label="Нажмите Enter">
-      <template slot="noResult" scope="props">Ничего не найдено</template>
-      <template slot="tag" scope="props">
-              <span class="multiselect__custom-tag">
-                <span class="multiselect__value" v-text="props.option.name"></span>
-                <span class="multiselect__custom-remove" @click="props.remove(props.option)"></span>
-              </span>
-      </template>
-    </multiselect>
-    <div style="display: none" ref="options">
+    <Multiselect
+      v-model="selectValue"
+      :options="options"
+      mode="tags"
+      :close-on-select="false"
+      :searchable="true"
+      placeholder="Выберите или начните вводить"
+      label="name"
+      value-prop="id"
+      track-by="name"
+      no-results-text="Ничего не найдено"
+    />
+    <div style="display: none" ref="optionsRef">
       <slot></slot>
     </div>
-    <input v-for="optionId in selectedIds" type="hidden" :name="name" :value="optionId">
-    <input v-if="selectedIds.length === 0" type="hidden" :name="name" value="">
+    <input v-for="optionId in selectValue" :key="optionId" type="hidden" :name="name" :value="optionId">
+    <input v-if="selectValue.length === 0" type="hidden" :name="name" value="">
   </div>
 </template>
 
-<script>
-  import Multiselect from 'vue-multiselect';
+<script setup>
+import { ref, onMounted } from 'vue'
+import Multiselect from '@vueform/multiselect'
 
-  export default {
-    data() {
-      return {
-        selectValue: [],
-        options: [],
-      }
-    },
-    mounted() {
-      const options = this.$refs.options.querySelectorAll('option');
-      options.forEach(option => {
-        const optionObject = {
-          id: option.value,
-          name: option.innerHTML
-        };
-
-        this.options.push(optionObject);
-        if(option.selected) {
-          this.selectValue.push(optionObject);
-        }
-      });
-    },
-    computed: {
-      selectedIds() {
-        if (this.selectValue.length === 0)
-          return [];
-        return this.selectValue.map((option) => option.id);
-      }
-    },
-    components: {
-      Multiselect
-    },
-    props: {
-      name: {
-        type: String,
-        required: true
-      },
-      id: {
-        type: String
-      }
-    }
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String
   }
+})
+
+const selectValue = ref([])
+const options = ref([])
+const optionsRef = ref(null)
+
+onMounted(() => {
+  const optionElements = optionsRef.value.querySelectorAll('option')
+  optionElements.forEach(option => {
+    const optionObject = {
+      id: option.value,
+      name: option.innerHTML
+    }
+    options.value.push(optionObject)
+    if (option.selected) {
+      selectValue.value.push(option.value)
+    }
+  })
+})
 </script>
--- app/frontend/components/SurveyForm/Question.vue
+++ app/frontend/components/SurveyForm/Question.vue
@@ -1,48 +1,45 @@
-<script>
-  export default {
-    data() {
-      return {
-        chosenOptions: [],
-        freeAnswer: null,
-      }
-    },
-    computed: {
-      freeAnswerChoosed() {
-        if(this.options.length == 0 && this.free)
-          return true;
-        return this.handledAnswers.indexOf("free") > -1;
-      },
-      handledAnswers() {
-        let options = [];
-        if(!this.multichoice) {
-          options.push(this.chosenOptions);
-        } else {
-          options = Object.assign([], this.chosenOptions);
-        }
-        return options;
-      }
-    },
-    watch: {
-      chosenOptions() {
-        this.$emit('input', {
-          options: this.handledAnswers.filter(Number),
-          text: this.freeAnswerChoosed ? this.freeAnswer : null
-        });
-      },
-      freeAnswer() {
-        this.$emit('input', {
-          options: this.handledAnswers.filter(Number),
-          text: this.freeAnswerChoosed ? this.freeAnswer : null
-        });
-      }
-    },
-    methods: {
-      optionID(id) {
-        return `option${id}`
-      }
-    },
-    props: ['required', 'text', 'multichoice', 'free', 'options', 'value']
+<script setup>
+import { ref, computed, watch } from 'vue'
+
+const props = defineProps({
+  required: Boolean,
+  text: String,
+  multichoice: Boolean,
+  free: Boolean,
+  options: Array,
+  modelValue: Object
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const chosenOptions = ref([])
+const freeAnswer = ref(null)
+
+const handledAnswers = computed(() => {
+  if (!props.multichoice) {
+    return [chosenOptions.value]
   }
+  return [...chosenOptions.value]
+})
+
+const freeAnswerChoosed = computed(() => {
+  if (props.options.length === 0 && props.free) return true
+  return handledAnswers.value.indexOf("free") > -1
+})
+
+function emitValue() {
+  emit('update:modelValue', {
+    options: handledAnswers.value.filter(Number),
+    text: freeAnswerChoosed.value ? freeAnswer.value : null
+  })
+}
+
+function optionID(id) {
+  return `option${id}`
+}
+
+watch(chosenOptions, emitValue, { deep: true })
+watch(freeAnswer, emitValue)
 </script>
 
 <template>
@@ -51,10 +48,10 @@
       <span v-if="required">* </span>{{ text }}
     </h3>
     <div class="survey-form-question__options">
-      <div class="survey-form-question__option" v-for="item in options">
+      <div class="survey-form-question__option" v-for="item in options" :key="item.id">
         <div class="survey-form-question__option-box">
-          <input type="checkbox" :id="optionID(item.id)" :value='item.id' v-model='chosenOptions' v-if='multichoice'>
-          <input type="radio" :id="optionID(item.id)" :value='item.id' v-model='chosenOptions' v-else>
+          <input type="checkbox" :id="optionID(item.id)" :value="item.id" v-model="chosenOptions" v-if="multichoice">
+          <input type="radio" :id="optionID(item.id)" :value="item.id" v-model="chosenOptions" v-else>
         </div>
         <label class="survey-form-question__option-label" :for="optionID(item.id)">
           {{ item.text }}
@@ -63,15 +60,15 @@
 
       <div class="survey-form-question__option" v-if="free && options.length > 0">
         <div class="survey-form-question__option-box">
-          <input type="checkbox" value='free' v-model='chosenOptions' v-if='multichoice'>
-          <input type="radio" value='free' v-model='chosenOptions' v-else>
+          <input type="checkbox" value="free" v-model="chosenOptions" v-if="multichoice">
+          <input type="radio" value="free" v-model="chosenOptions" v-else>
         </div>
         <div class="survey-form-question__option-text">
           <input type="text" v-model="freeAnswer" placeholder="Укажите Ваш ответ" :disabled="!freeAnswerChoosed">
         </div>
       </div>
 
-      <div class="survey-form-question__option" v-else-if="free && options.length == 0">
+      <div class="survey-form-question__option" v-else-if="free && options.length === 0">
         <div class="survey-form-question__option-text">
           <textarea v-model="freeAnswer" placeholder="Укажите Ваш ответ"></textarea>
         </div>
