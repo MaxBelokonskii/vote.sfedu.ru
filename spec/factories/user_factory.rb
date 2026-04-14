ce19ec694a65240a1039d744f2b7d57a5662735b
maxBelokonskii <m.belokonskiy@trucker.group>
2026-04-14 17:20:38 +0300

    Add tests for Survey model and improve user factory (7 examples)
    Survey: validations (title, passcode), answered_for?, nested attributes
    (create/destroy questions). User factory with teacher/student/admin
    sub-factories.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,4 +1,21 @@
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "user#{n}@sfedu.ru" }
+    sequence(:nickname) { |n| "user#{n}" }
+    sequence(:identity_url) { |n| "https://openid.sfedu.ru/server.php/idpage?user=user#{n}" }
+    role { :regular }
+
+    factory :user_with_teacher do
+      association :kind, factory: :teacher
+    end
+
+    factory :user_with_student do
+      association :kind, factory: :student
+    end
+
+    factory :admin_user do
+      role { :admin }
+      association :kind, factory: :student
+    end
   end
 end
